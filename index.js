70d03875c4acd893b0b02ceda3424467b1125ca3
Tony Lopes <tony@lopesdesign.com>
2017-03-13 12:06:15 -0700

    final amount I will get done today

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
+var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
@@ -51,24 +51,72 @@ Game.prototype.printBoard = function() {
   })
 }
 
-Game.prototype.play = function() {
+Game.prototype.play = function(coords) {
 
+  // process.stdout.write(coords);
 
-  // process.stdout.write(this.turn[1]);
+  // check if coordinates are in array form
+  // if not prompt for new input
+  // with helper text of 'needs to be an array'
+
+  // if they are, make sure they refer to an empty space
+  // if not prompt for new input
+  // with helper text of 'spot not valid. Played already or not a spot'
+
+  // if it's an empty spot
+  // place it on the board and return to turn flow.
+
 
 }
+
+
+Game.prototype.getUserMove = function() {
+
+  var messages = {
+    'notArray': 'please make your selection as coordinates in an array. For example: [1, 2]',
+    'spaceNotValid': 'chosen space not valid, please choose again'
+  }
+
+  this.thisQuestion = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  // console.log(this.thisQuestion)
+
+  this.thisQuestion.question(`${this.currentTurn[0]}, your turn: (choose [x, y]):`, answer => {
+    if (!Array.isArray(answer)) {
+
+      this.thisQuestion.close();
+
+      this.thisQuestion.question(messages['notArray'], answer => {
+
+        process.stdout.write(answer);
+      });
+    }
+
+  })
+
+
+  // rl.question(`${this.currentTurn[0]}, your turn: (choose [x, y]):`, answer => {
+
+  //   // this.play(answer);
+
+  //
+
+  // });
+
+}
+
+
 
 Game.prototype.turn = function() {
   // print the board
   this.printBoard();
 
   // prompt the current player for a move
+  this.getUserMove();
 
-  rl.question(`${this.currentTurn[0]}, your turn: (choose [x, y]):`, answer => {
-
-    console.log(answer);
-
-  });
 
   // player places a piece where there is a blank space
 
